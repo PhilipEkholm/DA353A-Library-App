24d6293d50711d5ff63d21e09c7647ddf8025666
Unknown <anasabualsoud@hotmail.com>
2017-02-27 14:07:39 +0100

    retrying

---
+++ Bibliotek/src/library/main.java
@@ -0,0 +1,18 @@
+package library;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.add(new LoginView());
+		frame.setPreferredSize(new Dimension(500, 200));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}

885c9a695b364b85ba91026ff467385e87637fe6
Philip Ekholm <philipekholm@protonmail.com>
2017-02-27 17:18:02 +0100

    Kompletterade en del filer

--- Bibliotek/src/library/Book.java
+++ Bibliotek/src/library/Book.java
@@ -1,7 +1,13 @@
 package library;
 
 public class Book extends Media{
-	public Book(String id){
-		super(id);
+	private String 	author,
+					bookTitle;
+	
+	public Book(String id, int year, String author, String bookTitle){
+		super(id, year);
+		
+		this.author = author;
+		this.bookTitle = bookTitle;
 	}
 }

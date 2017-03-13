a6444648ac4739790ff9639165cbda6a71e69d99
Unknown <henkefredlund@gmail.com>
2017-03-13 20:39:41 +0100

    Lite mer

--- Bibliotek/src/exchange/ItemsView.java
+++ Bibliotek/src/exchange/ItemsView.java
@@ -17,11 +17,16 @@ public class ItemsView extends JPanel{
 	private static final long serialVersionUID = 1L;
 	private JPanel 	headerPanel = new JPanel();
 	private JPanel libraryPanel = new JPanel();
-	private JTextField searchField = new JTextField("Sökfält");
+	private JPanel borrowPanel = new JPanel();
+	private JLabel searchLabel = new JLabel("Sök efter media:");
+	private JLabel borrowLabel = new JLabel("Ange media id:");
+	private JTextField searchField = new JTextField();
+	private JTextField borrowField = new JTextField();
 	private String[] comboString = { "Sök efter", "Media" , "Id" , "Författare" , 
 									"Titel" , "Utgivningsår", "Skådespelare" };
 	private JComboBox<String> searchOption =  new JComboBox<String>(comboString);
 	private JButton searchButton = new JButton("Sök");
+	private JButton borrowButton = new JButton("Låna");
 	private JTextArea area = new JTextArea("Library");
 	
 	
@@ -34,9 +39,13 @@ public class ItemsView extends JPanel{
 	}
 	
 	private void setDimensions(){
+		searchLabel.setPreferredSize(new Dimension(120,25));
 		searchField.setPreferredSize(new Dimension(200,25));
 		searchOption.setPreferredSize(new Dimension(150,25));
 		searchButton.setPreferredSize(new Dimension(100,25));
+		borrowLabel.setPreferredSize(new Dimension(120,25));
+		borrowField.setPreferredSize(new Dimension(150,25));
+		borrowButton.setPreferredSize(new Dimension(100,25));
 		area.setPreferredSize(new Dimension(700,500));
 		
 	}
@@ -46,12 +55,17 @@ public class ItemsView extends JPanel{
 	}
 	
 	private void addComponents(){
+		headerPanel.add(searchLabel);
 		headerPanel.add(searchField);
 		headerPanel.add(searchOption);
 		headerPanel.add(searchButton);
 		libraryPanel.add(area);
+		borrowPanel.add(borrowLabel);
+		borrowPanel.add(borrowField);
+		borrowPanel.add(borrowButton);
 		this.add(headerPanel, BorderLayout.NORTH);
 		this.add(libraryPanel, BorderLayout.CENTER);
+		this.add(borrowPanel, BorderLayout.SOUTH);
 	}
 	
 	private class AL implements ActionListener{
